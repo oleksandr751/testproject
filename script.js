8236ae25fb02292acfc1c7e5c9d354c0a6a059b6
Oleksandr <pecheinukoleksandr751@gmail.com>
2020-12-04 16:33:29 +0100

    Нихуя собі щей в..........................

--- script.js
+++ script.js
@@ -1,103 +1,124 @@
 let ListOfFilms = document.getElementById("ListOfFilms");
-let element = document.createElement("div");
-let element1 = document.createElement("div");
 let addnew = document.getElementById("NewFilm")
 let FilmYear = document.getElementById("AddFilmYear");
 let FilmName = document.getElementById("AddFilmName");
 let FilmStars = document.getElementById("AddFilmStars");
 let FilmFormat = document.getElementById("AddFilmFormat");
 let Submit11 = document.getElementById("Submit11");
+let element;
+let element1;
 let doc;
+let doc1;
 let inf;
 let inf1;
 let inf2;
 
 
 
-let List = [];
 
-let N = 100;
 
-List[0] = {
-    Title: "Troy",
-    ReleaseYear: "2004",
-    Format: "DVD",
-    Stars: "Brad Pitt, Eric Bana, Orlando Bloom, Diane Kruger"
-}
+let List = [{
+        Title: "Troy",
+        ReleaseYear: "2004",
+        Format: "DVD",
+        Stars: "Brad Pitt, Eric Bana, Orlando Bloom, Diane Kruger"
+    },
+    {
+        Title: "Raid",
+        ReleaseYear: "2011",
+        Format: "DVD",
+        Stars: "Iko Uwais, Joe Taslim, Yayan Ruhian, Donny Alamsyah"
+    }
+];
 
-List[1] = {
-    Title: "Raid",
-    ReleaseYear: "2011",
-    Format: "DVD",
-    Stars: "Iko Uwais, Joe Taslim, Yayan Ruhian, Donny Alamsyah"
-}
+
 
 function AddNewFilm() {
-    List[2].Title = FilmName.value;
-    List[2].ReleaseYear = FilmYear.value; //цю можеш не дивитись функцию там нижче 
-    List[2].Format = FilmFormat.value;
-    List[2].Stars = FilmStars.value;
-    console.log(List[2].Title)
+    let list = {
+        Title: FilmName.value,
+        ReleaseYear: FilmYear.value,
+        Format: FilmFormat.value,
+        Stars: FilmStars.value
+    }
+    ListOfFilms.innerHTML = "";
+    List.push(list);
+    ShowFilms();
+
+
 
 }
 
-for (let i = 0; i < 2; i++) {
+function ShowFilms() {
 
-    doc = document.createElement("p");
-    inf = document.createElement("p");
-    inf1 = document.createElement("p");
-    inf2 = document.createElement("p");
+    for (let i = 0; i < List.length; i++) {
 
-    doc.innerHTML = List[i].Title;
-    inf.innerHTML = List[i].ReleaseYear;
-    inf1.innerHTML = List[i].Format;
-    inf2.innerHTML = List[i].Stars;
+        element = document.createElement("div");
+        element.id = "div1"
+        element1 = document.createElement("div");
+        element1.id = "div2"
+        doc = document.createElement("p");
+        inf = document.createElement("p");
+        inf1 = document.createElement("p");
+        inf2 = document.createElement("p");
+        doc1 = document.createElement("button");
 
-    // doc.setAttribute("onclick", "onclick")
-    // doc.onclick = ToggleInfo();
+        // inf.hidden = true;
+        // inf1.hidden = true;
+        // inf2.hidden = true;
+
+        doc.innerHTML = List[i].Title;
+        doc1.innerHTML = "ShowInfo";
+        inf.innerHTML = List[i].ReleaseYear;
+        inf1.innerHTML = List[i].Format;
+        inf2.innerHTML = List[i].Stars;
+
+        // doc.setAttribute("onclick", "onclick")
+        // doc.onclick = ToggleInfo();
+
+
+        // doc.setAttribute("onclick")
+        // doc.onclick(inf.toggleAttribute("hidden"), inf1.toggleAttribute("hidden"), inf2.toggleAttribute("hidden"))
+
+        // element.append(doc);
+        // doc.append(doc1);
+        // element.append(inf);
+        // element.append(inf1);
+        // element.append(inf2);
+
+
+        element.append(doc);
+        element.append(doc1);
+        element.append(element1);
+        element1.append(inf);
+        element1.append(inf1);
+        element1.append(inf2);
+        ListOfFilms.append(element);
+
+        doc1.onclick = function() {
+            document.getElementById("div2").toggleAttribute("hidden");
+        }
 
 
 
-    element.append(doc);
-    element.append(inf);
-    element.append(inf1);
-    element.append(inf2);
-
-    inf.setAttribute("hidden", "hidden");
-    inf1.setAttribute("hidden", "hidden");
-    inf2.setAttribute("hidden", "hidden");
 
 
 
-    ListOfFilms.appendChild(element);
+
+        //ListOfFilms.appendChild(element);
+
+    }
 
 }
+
 
 
 function ToggleInfo() {
-    inf.toggleAttribute("hidden")
-    inf1.toggleAttribute("hidden")
-    inf2.toggleAttribute("hidden")
+    let a = document.getElementsByClassName("InfoF");
+
 }
 
 function Toggle() {
     addnew.toggleAttribute("hidden")
 }
 
-function AddNewFilm() {
-
-    console.log("Yobaniyrot")
-    let doc = document.createElement("p");
-    let inf = document.createElement("p");
-    let inf1 = document.createElement("p");
-    let inf2 = document.createElement("p");
-    doc.innerHTML = FilmName.value;
-    inf.innerHTML = FilmYear.value;
-    inf1.innerHTML = FilmFormat.value;
-    inf2.innerHTML = FilmStars.value;
-    ListOfFilms.appendChild(doc);
-    ListOfFilms.appendChild(inf);
-    ListOfFilms.appendChild(inf1);
-    ListOfFilms.appendChild(inf2);
-
-}
+ShowFilms();
